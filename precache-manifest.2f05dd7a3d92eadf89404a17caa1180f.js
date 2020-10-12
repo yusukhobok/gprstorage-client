5ce054dad647f83a05547bf76a3d5f11b5779518
root <root@DESKTOP-2V3CCNP.localdomain>
2020-10-12 18:24:31 +1000

    Updates

---
+++ precache-manifest.2f05dd7a3d92eadf89404a17caa1180f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050ab4d65d4ff59e62b8dccf1ef8f748",
+    "url": "/gprstorage-client/index.html"
+  },
+  {
+    "revision": "4e0ff2bae9b1de4f923a",
+    "url": "/gprstorage-client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a96752943f23fc4b8d1",
+    "url": "/gprstorage-client/static/js/2.7eed6e3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gprstorage-client/static/js/2.7eed6e3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0ff2bae9b1de4f923a",
+    "url": "/gprstorage-client/static/js/main.ca2b0da9.chunk.js"
+  },
+  {
+    "revision": "4c28f620d7771e541d4f",
+    "url": "/gprstorage-client/static/js/runtime-main.0e73e67e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gprstorage-client/static/media/logo.5d5d9eef.svg"
+  }
+]);
